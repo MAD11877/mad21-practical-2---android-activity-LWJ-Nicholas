1c99e7702fa25ec9c5b443512fc0c73aebeaf899
LWJ-Nicholas <S10222425@connect.np.edu.sg>
2022-05-03 23:52:25 +0800

    Practical 2 done

---
+++ app/src/main/java/sg/edu/np/mad/s10222425_practical_2/MainActivity.java
@@ -0,0 +1,70 @@
+package sg.edu.np.mad.s10222425_practical_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class User {
+        String name;
+        String description;
+        int id;
+        boolean followed;
+
+        public User(String n, String desc, int num){
+            name = n;
+            description = desc;
+            id = num;
+            followed = false;
+        }
+    }
+
+    //Creating a new user object
+    User student1 = new User("Nicholas Lee",
+            "Second year IT student at Ngee Ann Polytechnic, loves to play chess and talk to plants during his free time.",
+            10222425);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Retrieve name from user
+        //Display name
+        TextView nameTxt = findViewById(R.id.nameTxt);
+        nameTxt.setText(student1.name);
+
+        //Retrieve description from user
+        //Display description
+        TextView descTxt = findViewById(R.id.descTxt);
+        descTxt.setText(student1.description);
+
+        //Check if user is being followed
+        //Display unfollow/follow
+        Button followBtn = (Button)findViewById(R.id.followBtn);
+        if (student1.followed){
+            followBtn.setText(R.string.unfollow);
+        } else {
+            followBtn.setText(R.string.follow);
+        }
+    }
+
+    public void follow (View v){
+
+        //Check if user is being followed
+        //Display follow/unfollow
+        Button followBtn = (Button)findViewById(R.id.followBtn);
+        if (student1.followed){
+            followBtn.setText(R.string.follow);
+        } else {
+            followBtn.setText(R.string.unfollow);
+        }
+
+        //Update follow status accordingly
+        student1.followed = !student1.followed;
+    }
+}
