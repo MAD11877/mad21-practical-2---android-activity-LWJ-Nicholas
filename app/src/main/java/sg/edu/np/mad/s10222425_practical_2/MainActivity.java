866449cc2b9c230fc615ae504630fed2b29545c8
LWJ-Nicholas <S10222425@connect.np.edu.sg>
2022-05-04 01:12:46 +0800

    Completed Practical 2

--- app/src/main/java/sg/edu/np/mad/s10222425_practical_2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/s10222425_practical_2/MainActivity.java
@@ -9,16 +9,17 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Creating a new user object
-    User student1 = new User("Nicholas Lee",
-            "Second year IT student at Ngee Ann Polytechnic, loves to play chess and talk to plants during his free time.",
-            10222425,
-            false);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Creating a new user object
+        //Is it recommended that we extract string resource for the name and description?
+        User student1 = new User("Nicholas Lee",
+                "Second year IT student at Ngee Ann Polytechnic, loves to play chess and talk to plants during his free time.",
+                10222425,
+                false);
 
         //Retrieve name from user
         //Display name
@@ -38,20 +39,25 @@ public class MainActivity extends AppCompatActivity {
         } else {
             followBtn.setText(R.string.follow);
         }
+
+        //Change the status of "followed" whenever the follow button is clicked
+        followBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Check if user is being followed
+                //Update the text displayed on the follow button
+                Button followBtn = findViewById(R.id.followBtn);
+                if (student1.followed){
+                    followBtn.setText(R.string.follow);
+                } else {
+                    followBtn.setText(R.string.unfollow);
+                }
+
+                //Update followed status accordingly
+                student1.followed = !student1.followed;
+            }
+        });
     }
 
-    public void follow (View v){
-
-        //Check if user is being followed
-        //Display follow/unfollow
-        Button followBtn = findViewById(R.id.followBtn);
-        if (student1.followed){
-            followBtn.setText(R.string.follow);
-        } else {
-            followBtn.setText(R.string.unfollow);
-        }
-
-        //Update follow status accordingly
-        student1.followed = !student1.followed;
-    }
 }
